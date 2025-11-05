7701d87ce29e7216a36d2c1ad9d9486b25008509
kali <Ericferraz4@gmail.com>
2025-11-05 20:03:23 -0300

    arrumando val stataus Lending

--- src/main/kotlin/dev/weg/alfa/modules/models/simpleModels/LendingStatus.kt
+++ src/main/kotlin/dev/weg/alfa/modules/models/simpleModels/LendingStatus.kt
@@ -14,5 +14,5 @@ data class LendingStatus (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int,
-    val lendingStatus: String,
+    val status_lending: String,
     )
